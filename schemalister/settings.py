6128f2ddff4842f146452bd0a725c568065f2d36
Rick Pearson <pwpearson@gmail.com>
2019-12-08 08:10:40 -0500

    Added db password

--- schemalister/settings.py
+++ schemalister/settings.py
@@ -68,6 +68,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'postgres',
         'USER': 'postgres',
+        'PASSWORD': 'postgres',
         'HOST': 'db',
         'PORT': 5432,
     }
